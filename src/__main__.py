87a714840a79552fa8acd525dc0d8d436c88d6f0
Maximilian Prusch <maximilian.prusch@outlook.com>
2019-11-25 23:13:55 +0100

    Upload bands to s3

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,19 @@
 from typing import List
 import requests
+import boto3
+import logging
+import json
+
+from botocore.exceptions import ClientError
 
 ARTISTS_URL = 'https://www.wacken.com/de/programm/bands/?type=1541083944&tx_woamanager_pi2%5Bfestival%5D=4&tx_woamanager_pi2%5Bperformance%5D=1%2C7&tx_woamanager_pi2%5Baction%5D=list&tx_woamanager_pi2%5Bcontroller%5D=AssetJson&cHash=4aaeb0a4c6c3f83fbdd4013abb42357d'
+BUCKET_NAME = 'festival-bands-dev'
+BUCKET_KEY = '/wacken.json'
+S3_CLIENT = boto3.client('s3')
 
 
 def get_bands_handler(event, context):
-    return get_bands()
+    upload_to_s3(get_bands())
 
 
 def get_bands() -> List[str]:
@@ -17,3 +25,11 @@ def get_bands() -> List[str]:
             band_names.append(band['artist']['title'])
 
     return band_names
+
+
+def upload_to_s3(bands: List[str]):
+    if bands:
+        try:
+            S3_CLIENT.put_object(Bucket=BUCKET_NAME, Key=BUCKET_KEY, Body=json.dumps(bands))
+        except ClientError as e:
+            logging.error(e)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,8 @@
 import responses
+import json
 
-from src.__main__ import get_bands, ARTISTS_URL
+from botocore.stub import Stubber
+from src.__main__ import get_bands, ARTISTS_URL, upload_to_s3, S3_CLIENT, BUCKET_NAME, BUCKET_KEY
 
 
 @responses.activate
@@ -19,3 +21,30 @@ def test_get_bands_returns_list_of_bands():
 def test_get_bands_returns_empty_list_when_request_is_not_successful():
     responses.add(responses.GET, ARTISTS_URL, status=500)
     assert get_bands() == []
+
+
+def test_upload_to_s3_uploads_list_of_bands():
+    bands = ['Bloodbath', 'Megadeth', 'Vader']
+    with Stubber(S3_CLIENT) as s3_stub:
+        s3_stub.add_response('put_object', {},
+                             {'Body': json.dumps(bands), 'Bucket': BUCKET_NAME, 'Key': BUCKET_KEY})
+        upload_to_s3(bands)
+
+    assert s3_stub.assert_no_pending_responses
+
+
+def test_upload_to_s3_without_empty_list_does_not_upload():
+    with Stubber(S3_CLIENT) as s3_stub:
+        upload_to_s3([])
+
+    assert s3_stub.assert_no_pending_responses
+
+
+def test_upload_to_s3_logs_exception(caplog):
+    with Stubber(S3_CLIENT) as s3_stub:
+        s3_stub.add_client_error('put_object', service_error_code='DEAD', service_message='BUCKET DEAD')
+        upload_to_s3(['band'])
+
+    for record in caplog.records:
+        assert record.levelname == 'ERROR'
+        assert record.getMessage() == 'An error occurred (DEAD) when calling the PutObject operation: BUCKET DEAD'
